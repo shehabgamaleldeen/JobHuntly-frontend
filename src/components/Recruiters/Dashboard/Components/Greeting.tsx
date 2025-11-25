1c2301ae5ec516b44d9753b2c5872a6fd09fa771
Ahmed Mekheimer <ahmed.khaled.mekheimer@gmail.com>
2025-11-25 21:14:42 +0200

    First Company Dashboard components (Still not responsive or user input reactive) 1- Greeting 2- Statistics Cards

---
+++ src/components/Recruiters/Dashboard/Components/Greeting.tsx
@@ -0,0 +1,14 @@
+export default function Greeting() {
+    return (
+        <>
+            <main className="p-8">
+                <section className="flex flex-col gap-2 w-max h-max">
+                    <h4 className="font-semibold text-2xl text-[#25324B]">Good Morning</h4>
+                    <p className="font-medium text-base text-[#7C8493]">
+                        Here's your job listings statistics report
+                    </p>
+                </section>
+            </main>
+        </>
+    );
+}
