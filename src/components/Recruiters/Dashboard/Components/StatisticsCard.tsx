2689916c93112ee0363be5cd1360eaa9d745f874
Ahmed Mekheimer <ahmed.khaled.mekheimer@gmail.com>
2025-11-26 00:27:33 +0200

    1- Chart added with needed libraries 2- Side cards added 3- Events fire from buttons 4- Unified headings and buttons css

--- src/components/Recruiters/Dashboard/Components/StatisticsCard.tsx
+++ src/components/Recruiters/Dashboard/Components/StatisticsCard.tsx
@@ -2,7 +2,7 @@ export default function StatisticCard({title, bgColor}: {title?: string, bgColor
     return (
         <>
             <main className={`text-white p-6 w-1/3 flex gap-3 items-center ${bgColor}`}>
-                <h2 className="font-semibold text-5xl">70</h2>
+                <h1>70</h1>
                 <p>{title}</p>
             </main>
         </>
