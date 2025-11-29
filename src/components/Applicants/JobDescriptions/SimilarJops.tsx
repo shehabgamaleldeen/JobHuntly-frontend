713d8a7760816662ad9921a7e12109ac35c03628
000Shehab000 <shhab200025@gmail.com>
2025-11-29 19:14:22 +0200

    feat(applicants): create SimilarJopCard component
    - Created SimilarJopCard component
    - Replaced static data with component

--- src/components/Applicants/JobDescriptions/SimilarJops.tsx
+++ src/components/Applicants/JobDescriptions/SimilarJops.tsx
@@ -1,3 +1,5 @@
+import SimilarJopCard from './Cards/SimilarJopCard'
+
 const SimilarJops = () => {
   return (
     <>
@@ -6,110 +8,12 @@ const SimilarJops = () => {
           Similiar Jobs
         </h2>
         <div className="grid grid-cols-2 max-md:grid-cols-1 max-sm:grid-cols-1 gap-8 mb-8">
-          <article className="bg-[#FFFFFF] flex max-xl:flex-col max-lg:flex-col max-sm:flex-col justify-center p-6 gap-3">
-            <img
-              className="w-12 h-14"
-              src="/SimilarJopIcon.png"
-              alt="jopIcon"
-            />
-            <div>
-              <h3 className="text-xl text-[#25324B] font-semibold mb-2">
-                Social Media Assistant
-              </h3>
-              <span className=" text-[#515B6F] my-2">
-                Nomad . Paris, France
-              </span>
-              <div>
-                <span className="text-[#FFB836] bg-[#EB85331A] w-24 inline-block text-base font-semibold rounded-3xl text-center my-4 mr-4">
-                  Marketing
-                </span>
-                <span className="text-[#56CDAD] bg-[#56CDAD1A] w-24 inline-block text-base font-semibold rounded-3xl text-center my-4 mr-4">
-                  Design
-                </span>
-                <span className="text-[#4640DE] p-2 bg-[#F8F8FD] w-fit inline-block text-base font-semibold rounded-3xl text-center my-1 mr-4">
-                  Project Management
-                </span>
-              </div>
-            </div>
-          </article>
-          <article className="bg-[#FFFFFF] flex max-xl:flex-col max-lg:flex-col max-sm:flex-col justify-center p-6 gap-3">
-            <img
-              className="w-12 h-14"
-              src="/SimilarJopIcon.png"
-              alt="jopIcon"
-            />
-            <div>
-              <h3 className="text-xl text-[#25324B] font-semibold mb-2">
-                Social Media Assistant
-              </h3>
-              <span className=" text-[#515B6F] my-2">
-                Nomad . Paris, France
-              </span>
-              <div>
-                <span className="text-[#FFB836] bg-[#EB85331A] w-24 inline-block text-base font-semibold rounded-3xl text-center my-4 mr-4">
-                  Marketing
-                </span>
-                <span className="text-[#56CDAD] bg-[#56CDAD1A] w-24 inline-block text-base font-semibold rounded-3xl text-center my-4 mr-4">
-                  Design
-                </span>
-                <span className="text-[#4640DE] p-2 bg-[#F8F8FD] w-fit inline-block text-base font-semibold rounded-3xl text-center my-1 mr-4">
-                  Project Management
-                </span>
-              </div>
-            </div>
-          </article>
-          <article className="bg-[#FFFFFF] flex max-xl:flex-col max-lg:flex-col max-sm:flex-col justify-center p-6 gap-3">
-            <img
-              className="w-12 h-14"
-              src="/SimilarJopIcon.png"
-              alt="jopIcon"
-            />
-            <div>
-              <h3 className="text-xl text-[#25324B] font-semibold mb-2">
-                Social Media Assistant
-              </h3>
-              <span className=" text-[#515B6F] my-2">
-                Nomad . Paris, France
-              </span>
-              <div>
-                <span className="text-[#FFB836] bg-[#EB85331A] w-24 inline-block text-base font-semibold rounded-3xl text-center my-4 mr-4">
-                  Marketing
-                </span>
-                <span className="text-[#56CDAD] bg-[#56CDAD1A] w-24 inline-block text-base font-semibold rounded-3xl text-center my-4 mr-4">
-                  Design
-                </span>
-                <span className="text-[#4640DE] p-2 bg-[#F8F8FD] w-fit inline-block text-base font-semibold rounded-3xl text-center my-1 mr-4">
-                  Project Management
-                </span>
-              </div>
-            </div>
-          </article>
-          <article className="bg-[#FFFFFF] flex max-xl:flex-col max-lg:flex-col max-sm:flex-col justify-center p-6 gap-3">
-            <img
-              className="w-12 h-14"
-              src="/SimilarJopIcon.png"
-              alt="jopIcon"
-            />
-            <div>
-              <h3 className="text-xl text-[#25324B] font-semibold mb-2">
-                Social Media Assistant
-              </h3>
-              <span className=" text-[#515B6F] my-2">
-                Nomad . Paris, France
-              </span>
-              <div>
-                <span className="text-[#FFB836] bg-[#EB85331A] w-24 inline-block text-base font-semibold rounded-3xl text-center my-4 mr-4">
-                  Marketing
-                </span>
-                <span className="text-[#56CDAD] bg-[#56CDAD1A] w-24 inline-block text-base font-semibold rounded-3xl text-center my-4 mr-4">
-                  Design
-                </span>
-                <span className="text-[#4640DE] p-2 bg-[#F8F8FD] w-fit inline-block text-base font-semibold rounded-3xl text-center my-1 mr-4">
-                  Project Management
-                </span>
-              </div>
-            </div>
-          </article>
+          <SimilarJopCard />
+          <SimilarJopCard />
+          <SimilarJopCard />
+          <SimilarJopCard />
+          <SimilarJopCard />
+          <SimilarJopCard />
         </div>
       </section>
     </>
