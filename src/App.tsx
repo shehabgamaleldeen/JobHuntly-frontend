932858dba37ae9ba2c68882e5763e57506aac334
Ahmed Mekheimer <ahmed.khaled.mekheimer@gmail.com>
2025-11-30 20:58:42 +0200

    Routes, Company Layout, Organized Dashboard page elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Basic/Navbar/Navbar';
 import Footer from './components/Basic/footer/footer';
@@ -9,12 +8,17 @@ import Signup from './components/Applicants/Signup/Signup';
 import SearchCompanies from './components/Applicants/SearchCompanies/SearchCompanies';
 import NotFoundPage from './components/Basic/NotFoundPage';
 import './App.css';
+import CompanyLayout from './components/Recruiters/CompanyLayout';
+import JobCreateLayout from './components/Recruiters/JobCreate/JobCreateLayout';
+import Step1 from './components/Recruiters/JobCreate/Components/Steps/Step1';
+import Step2 from './components/Recruiters/JobCreate/Components/Steps/Step2';
+import Step3 from './components/Recruiters/JobCreate/Components/Steps/Step3';
 import CompanyDashboard from './components/Recruiters/Dashboard/Dashboard';
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-50 flex flex-col">
+      <div className="min-h-screen flex flex-col">
         <Navbar />
         <main className="flex-grow">
           <Routes>
@@ -24,7 +28,17 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="*" element={<NotFoundPage />} />
-            <Route path="/company" element={<CompanyDashboard />} />
+
+            <Route path="/company" element={<CompanyLayout />}>
+              <Route index element={<CompanyDashboard />} />
+
+              <Route path="job-create" element={<JobCreateLayout />}>
+                <Route index element={<Step1 />} />
+                <Route path="step-1" element={<Step1 />} />
+                <Route path="step-2" element={<Step2 />} />
+                <Route path="step-3" element={<Step3 />} />
+              </Route>
+            </Route>
           </Routes>
         </main>
         <Footer />
