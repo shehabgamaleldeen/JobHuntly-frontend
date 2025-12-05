815577ce09d9126849a035fee58c9cf487206969
mouis666666 <moaz666666@outlook.com>
2025-12-06 01:08:05 +0200

    add all settings and company profile and sidenav and jobpost nav and more

---
+++ src/components/Recruiters/DashboardCompanyProfile/CompanyHeader/CompanyStats.tsx
@@ -0,0 +1,21 @@
+import { faCalendarAlt, faUsers, faMapMarkerAlt, faBuilding } from "@fortawesome/free-solid-svg-icons";
+import DetailItem from "./DetailItem";
+
+interface Props {
+  founded: string;
+  employees: string;
+  location: number;
+  industry: string;
+}
+
+function CompanyStats({ founded, employees, location, industry }: Props) {
+  return (
+    <div className="flex flex-wrap gap-10 justify-items-stretch pt-3 font-epilogue text-base text-slate-600">
+      <DetailItem icon={faCalendarAlt} label="Founded" value={founded} />
+      <DetailItem icon={faUsers} label="Employees" value={employees} />
+      <DetailItem icon={faMapMarkerAlt} label="Location" value={location?.toLocaleString() + " Countries"} />
+      <DetailItem icon={faBuilding} label="Industry" value={industry} />
+    </div>
+  );
+}
+export default CompanyStats;
