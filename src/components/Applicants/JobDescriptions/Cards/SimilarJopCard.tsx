2a130cb8dcbddd422e2ae1151b69ded38ce13bfc
000Shehab000 <shhab200025@gmail.com>
2025-12-19 22:25:50 +0200

    refactor: improve styling for ApplyButton and SimilarJopCard components

--- src/components/Applicants/JobDescriptions/Cards/SimilarJopCard.tsx
+++ src/components/Applicants/JobDescriptions/Cards/SimilarJopCard.tsx
@@ -1,7 +1,7 @@
 const SimilarJopCard = () => {
   return (
     <>
-      <article className="bg-[#FFFFFF] flex max-xl:flex-col max-lg:flex-col max-sm:flex-col justify-center p-6 gap-3">
+      <article className="bg-[#FFFFFF] flex max-xl:flex-col max-lg:flex-col max-sm:flex-col p-6 gap-3">
         <img className="w-12 h-14" src="/SimilarJopIcon.png" alt="jopIcon" />
         <div>
           <h3 className="text-xl text-[#25324B] font-semibold mb-2">
@@ -9,10 +9,10 @@ const SimilarJopCard = () => {
           </h3>
           <span className=" text-[#515B6F] my-2">Nomad . Paris, France</span>
           <div>
-            <span className="text-[#FFB836] bg-[#EB85331A] w-24 inline-block text-base font-semibold rounded-3xl text-center my-4 mr-4">
+            <span className="text-[#FFB836] bg-[#EB85331A] w-fit inline-block text-base font-semibold rounded-3xl text-center mt-4 mr-4 p-2">
               Marketing
             </span>
-            <span className="text-[#56CDAD] bg-[#56CDAD1A] w-24 inline-block text-base font-semibold rounded-3xl text-center my-4 mr-4">
+            <span className="text-[#56CDAD] bg-[#56CDAD1A] w-fit inline-block text-base font-semibold rounded-3xl text-center mt-4 mr-4 p-2 ">
               Design
             </span>
             <span className="text-[#4640DE] p-2 bg-[#F8F8FD] w-fit inline-block text-base font-semibold rounded-3xl text-center my-1 mr-4">
