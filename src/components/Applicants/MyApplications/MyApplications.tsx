0b63e89d4bdd2e97af192939d4d29c0b57d9fa29
MoazRyhan <moazryhan@gmail.com>
2025-12-30 01:56:54 +0200

    handle the nav ber and make most of changes for ui before intrate with back    2 fix mariz error 3 hadle the outlet for the project sidenavbar

--- src/components/Applicants/MyApplications/MyApplications.tsx
+++ src/components/Applicants/MyApplications/MyApplications.tsx
@@ -1,17 +1,19 @@
 import { useState } from "react";
 import ApplicationTable from "./ApplicationTable";
 import SearchBar from "./SearchBar";
+import { PageHeader } from "../DashboardSettings/headParts/headerPart";
 
 export default function MyApplications() {
   const [searchText, setSearchText] = useState("");
 
   return (
+    <>
+    <PageHeader 
+        title="My Applications"     
+        buttonText="Back to homepage"
+        buttonLink="/Dashboard" />
     <div className="flex min-h-screen bg-white">
-      <div className="flex-1 w-full px-4 py-6 md:px-6 md:py-8 lg:px-8 lg:py-8">
-        <div className="mb-6">
-          <h2 className="font-bold text-[#25324B] text-xl md:text-2xl">Keep it up!</h2>
-        </div>
-
+      <div className="flex-1 w-full px-4 py-6 md:px-6 md:py-8 lg:px-8 lg:py-8">        
         <div className="w-full max-w-[1104px] mx-auto">
           <div className="flex justify-between items-center mb-4">
             <h2 className="font-bold text-[#25324B] text-xl md:text-2xl">Application History</h2>
@@ -24,5 +26,6 @@ export default function MyApplications() {
         </div>
       </div>
     </div>
+    </>
   );
 }
