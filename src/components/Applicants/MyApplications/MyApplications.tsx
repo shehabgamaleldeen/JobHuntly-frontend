4bb4051cf728081e26f7fbe089f3f4e402273729
Mariz Youssef <mariz.youssef7@gamil.com>
2025-12-31 10:28:54 +0200

    my applications table

--- src/components/Applicants/MyApplications/MyApplications.tsx
+++ src/components/Applicants/MyApplications/MyApplications.tsx
@@ -8,24 +8,27 @@ export default function MyApplications() {
 
   return (
     <>
-    <PageHeader 
+      <PageHeader 
         title="My Applications"     
         buttonText="Back to homepage"
-        buttonLink="/Dashboard" />
-    <div className="flex min-h-screen bg-white">
-      <div className="flex-1 w-full px-4 py-6 md:px-6 md:py-8 lg:px-8 lg:py-8">        
-        <div className="w-full max-w-[1104px] mx-auto">
-          <div className="flex justify-between items-center mb-4">
-            <h2 className="font-bold text-[#25324B] text-xl md:text-2xl">Application History</h2>
-            <SearchBar onSearch={(query) => setSearchText(query)} />
-          </div>
+        buttonLink="/Dashboard" 
+      />
+      <div className="flex min-h-screen bg-white">
+        <div className="flex-1 w-full px-4 py-6 md:px-6 md:py-8 lg:px-8 lg:py-8">        
+          <div className="w-full max-w-[1104px] mx-auto">
+            <div className="flex justify-between items-center mb-4">
+              <h2 className="font-bold text-[#25324B] text-xl md:text-2xl">
+                Application History
+              </h2>
+              <SearchBar onSearch={(query) => setSearchText(query)} />
+            </div>
 
-          <div className="w-full overflow-x-auto">
-            <ApplicationTable userId="user_applicant_001" searchText={searchText} />
+            <div className="w-full overflow-x-auto">
+              <ApplicationTable searchText={searchText} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
