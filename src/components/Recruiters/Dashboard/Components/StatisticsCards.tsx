393fc21ad60457b8d5dd3aa74a861f40bc5af58c
Ahmed Mekheimer <ahmed.khaled.mekheimer@gmail.com>
2026-01-02 21:13:40 +0200

    Refactors: 1- Job Statistics Chart including all data for cards and graph 2- Dashboard Cards filled 3- Fetched dashboard APIs

--- src/components/Recruiters/Dashboard/Components/StatisticsCards.tsx
+++ src/components/Recruiters/Dashboard/Components/StatisticsCards.tsx
@@ -1,6 +1,29 @@
+import { useEffect, useState } from "react";
 import StatisticsCard from "./StatisticCard";
+import { getNewJobApplicationsCount, getOpenJobsCount, getReviewedJobApplicationsCount } from "@/services/companyDashboardService";
 
 export default function StatisticsCards() {
+    const [newCandidates, setNewCandidates] = useState(0)
+    const [reviewedCandidates, setReviewedCandidates] = useState(0)
+    const [jobsOpen, setJobsOpen] = useState(0)
+
+    useEffect(() => {
+        const fetchCards = async () => {
+            try {
+                const [NewCandidates, ReviewedCandidates, JobsOpen] = await Promise.all([
+                    getNewJobApplicationsCount(),
+                    getReviewedJobApplicationsCount(),
+                    getOpenJobsCount()
+                ]);
+                setNewCandidates(NewCandidates.data.data);
+                setReviewedCandidates(ReviewedCandidates.data.data);
+                setJobsOpen(JobsOpen.data.data);
+            } catch (err) {
+                console.error("Dashboard Cards Fetch Error:", err);
+            }
+        };
+        fetchCards();
+    }, []);
 
     const containerClasses = `
         flex flex-col items-center gap-4 mx-8
@@ -18,15 +41,15 @@ export default function StatisticsCards() {
         <div className={containerClasses}>
 
             <section className={cardWidthClasses}>
-                <StatisticsCard title="New candidates to review" bgColor="bg-[#4640DE]" />
+                <StatisticsCard title="New candidates to review" bgColor="bg-[#4640DE]" count={newCandidates}/>
             </section>
 
             <section className={cardWidthClasses}>
-                <StatisticsCard title="Candidates reviewed today" bgColor="bg-[#56CDAD]" />
+                <StatisticsCard title="Candidates reviewed today" bgColor="bg-[#56CDAD]" count={reviewedCandidates}/>
             </section>
 
             <section className={cardWidthClasses}>
-                <StatisticsCard title="Jobs Opened" bgColor="bg-[#26A4FF]" />
+                <StatisticsCard title="Jobs Opened" bgColor="bg-[#26A4FF]" count={jobsOpen}/>
             </section>
 
         </div>
