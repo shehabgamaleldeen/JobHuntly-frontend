9e1bab29aea8d294c9f382bca9f5501f32d0d634
MoazRyhan <moazryhan@gmail.com>
2026-01-10 00:55:52 +0200

    add the back

--- src/components/AxiosConfig/instance.ts
+++ src/components/AxiosConfig/instance.ts
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://jobhuntly-backend-production.up.railway.app',
 })
 
 export const refreshClient = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://jobhuntly-backend-production.up.railway.app",
 });
 
 const getAuthStorage = () => {
